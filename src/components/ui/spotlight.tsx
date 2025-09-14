49fa26c4c166ce136df1c4270a24b616671b0e33
Self Improvement <improvements363@gmail.com>
2025-09-14 13:43:50 +0000

    Copy-paste this component to /components/ui folder: ```tsx spotlight.tsx

--- src/components/ui/spotlight.tsx
+++ src/components/ui/spotlight.tsx
@@ -1,7 +1,5 @@
-'use client';
-
-import { cn } from '@/lib/utils';
-import React from 'react';
+import React from "react";
+import { cn } from "@/lib/utils";
 
 type SpotlightProps = {
   className?: string;
@@ -12,7 +10,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        'animate-spotlight pointer-events-none absolute z-[1] h-[169%] w-[138%] lg:w-[84%]',
+        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] w-[138%] lg:w-[84%] opacity-0",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
@@ -25,8 +23,8 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           cy="273.501"
           rx="1924.71"
           ry="273.501"
-          transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 1632.51 2108.43)"
-          fill={fill || 'white'}
+          transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
+          fill={fill || "white"}
           fillOpacity="0.21"
         ></ellipse>
       </g>
