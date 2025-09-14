c639dd9f4f33385d54c2b46d37047f90a74d04b4
Self Improvement <improvements363@gmail.com>
2025-09-14 13:26:59 +0000

    Current State Analysis:
    Website has a dark black/charcoal background
    Cl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="relative flex h-dvh flex-col bg-black">
       <Header />
       <div className="absolute inset-0 -z-10 bg-gradient-radial-hero" />
+      <div className="background-lighting" />
       <main className="flex-1">
         <SplineSceneBasic />
       </main>
