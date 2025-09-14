604f9a69ecbd6436c250caae3ff9a0f6ea70a033
Self Improvement <improvements363@gmail.com>
2025-09-14 09:17:18 +0000

    you have removed the navbar now bring the  navbar back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/layout/header';
 import { SplineSceneBasic } from '@/components/ui/splite';
 
 export default function Home() {
   return (
     <div className="flex h-dvh flex-col bg-black">
+      <Header />
       <main className="flex-1">
         <SplineSceneBasic />
       </main>
