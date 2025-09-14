6297a676a7da8f25e09af360cd28ee5333d18204
Self Improvement <improvements363@gmail.com>
2025-09-14 13:59:56 +0000

    make the hero section fill the whole screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,18 @@ import Hero from '@/components/sections/hero';
 
 export default function Home() {
   return (
-    <div className="flex min-h-dvh flex-col bg-background">
-      <Header />
-      <main className="flex-1 pt-20">
-        <Hero />
-        <Features />
-        <ProductPreview />
-      </main>
-      <Footer />
+    <div className="flex min-h-dvh flex-col bg-black">
+      <Hero />
+      <div className="relative z-10 flex flex-1 flex-col">
+        <Header />
+        <main className="flex-1 bg-transparent">
+          {/* Add a spacer to push content below the header */}
+          <div className="h-20" />
+          <Features />
+          <ProductPreview />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
