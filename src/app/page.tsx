5852970b4964da26523e7fa1d76a3401e44fa107
Self Improvement <improvements363@gmail.com>
2025-09-14 13:14:31 +0000

    you have removed the background  limelight which was coming from the lef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import { SplineSceneBasic } from '@/components/ui/splite';
 
 export default function Home() {
   return (
-    <div className="flex h-dvh flex-col bg-black">
+    <div className="relative flex h-dvh flex-col bg-black">
       <Header />
+      <div className="absolute inset-0 -z-10 bg-gradient-radial-hero" />
       <main className="flex-1">
         <SplineSceneBasic />
       </main>
